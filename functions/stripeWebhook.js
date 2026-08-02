b4f6b1d67c888d79284b7ac6b1afbe0fde5db406
Dave Liu <7david12liu@gmail.com>
2026-08-01 20:21:19 -0700

    fix(stripe): reject impossible dispute chronology (#594)

--- functions/stripeWebhook.js
+++ functions/stripeWebhook.js
@@ -181,6 +181,14 @@ function validateEventAdmission(event, expectedLivemode) {
       reason: 'invalid_dispute_created',
     };
   }
+  if (DISPUTE_EVENT_TYPES.has(event.type)
+    && event.data.object.created > event.created) {
+    return {
+      ok: false,
+      expected: expectedLivemode,
+      reason: 'invalid_dispute_event_chronology',
+    };
+  }
   if (CHECKOUT_EVENT_TYPES.has(event.type)
     && !isValidStripeCreated(event.data?.object?.created)) {
     return {
